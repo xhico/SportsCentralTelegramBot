89019d5a21a8a3d9eb8618e1febb70e494b5eeb0
xhico <francgf@gmail.com>
2022-08-20 21:44:57 +0100

    Update SoccerStreamsNotifications.py

--- SoccerStreamsNotifications.py
+++ SoccerStreamsNotifications.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
 
-# python3 -m pip install yagmail psutil --no-cache-dir
+# python3 -m pip install yagmail psutil python-telegram-bot --no-cache-dir
 import datetime
 import json
 import os
 import traceback
 from operator import getitem
 
-import yagmail
 import psutil
 import requests
 from collections import OrderedDict
+
+import yagmail
+from telegram import Bot
 
 
 def get911(key):
@@ -23,6 +25,8 @@ def get911(key):
 EMAIL_USER = get911('EMAIL_USER')
 EMAIL_APPPW = get911('EMAIL_APPPW')
 EMAIL_RECEIVER = get911('EMAIL_RECEIVER')
+TELEGRAM_TOKEN = get911('TELEGRAM_TOKEN')
+TELEGRAM_USER_ID = get911('TELEGRAM_USER_ID')
 
 
 def getLog():
@@ -48,16 +52,20 @@ def getMatches():
     allJSON = {"yesterday": yesterdayJSON, "today": todayJSON, "tomorrow": tomorrowJSON}
 
     # Iterate over every event
-    for event in [event for dayJSON in allJSON.values() for league in dayJSON for event in league["events"]]:
-        eventId = event["id"]
-        homeTeam = str(event["homeTeam"]["name"])
-        awayTeam = str(event["awayTeam"]["name"])
-        homeScore = event["homeScore"]["current"]
-        awayScore = event["awayScore"]["current"]
-        minute = event["minute"]
-        status = "halftime" if str(event["statusDescription"]) == "HF" else str(event["status"]["type"])
-        startTimestamp = event["startTimestamp"]
-        matches[eventId] = {"homeTeam": homeTeam, "awayTeam": awayTeam, "homeScore": homeScore, "awayScore": awayScore, "minute": minute, "status": status, "startTimestamp": startTimestamp}
+    # for event in [event for dayJSON in allJSON.values() for league in dayJSON for event in league["events"]]:
+    for dayJSON in allJSON.values():
+        for league in dayJSON:
+            for event in league["events"]:
+                eventId = event["id"]
+                leagueName = league["name"]
+                homeTeam = str(event["homeTeam"]["name"])
+                awayTeam = str(event["awayTeam"]["name"])
+                homeScore = event["homeScore"]["current"]
+                awayScore = event["awayScore"]["current"]
+                minute = event["minute"]
+                status = "halftime" if str(event["statusDescription"]) == "HF" else str(event["status"]["type"])
+                startTimestamp = event["startTimestamp"]
+                matches[eventId] = {"leagueName": leagueName, "homeTeam": homeTeam, "awayTeam": awayTeam, "homeScore": homeScore, "awayScore": awayScore, "minute": minute, "status": status, "startTimestamp": startTimestamp}
 
     # Sort matches by startTimestamp
     matches = dict(OrderedDict(sorted(matches.items(), key=lambda x: getitem(x[1], 'startTimestamp'))))
@@ -74,52 +82,71 @@ def main():
         eventId = str(eventId)
 
         # Get match info
-        homeScore, awayScore, status = match["homeScore"], match["awayScore"], match["status"]
+        homeScore, awayScore, status, leagueName = match["homeScore"], match["awayScore"], match["status"], match["leagueName"]
         homeTeam, awayTeam, minute = match["homeTeam"], match["awayTeam"], match["minute"]
+        messageText = False
 
         # If match not in JSON_REPORT -> add
         if eventId not in JSON_REPORT and status != "finished":
             print(eventId, "ADD")
+            match["messageId"] = False
             JSON_REPORT[eventId] = match
 
         # If match is already in JSON_REPORT -> check for changes
         if eventId in JSON_REPORT:
+
+            # Check for LOG_messageId
+            try:
+                LOG_messageId = JSON_REPORT[eventId]["messageId"]
+                match["messageId"] = LOG_messageId
+            except KeyError:
+                LOG_messageId = False
 
             # Get LOG info
             LOG_homeScore, LOG_awayScore, LOG_status = JSON_REPORT[eventId]["homeScore"], JSON_REPORT[eventId]["awayScore"], JSON_REPORT[eventId]["status"]
 
             # Check if match has started
             if LOG_status == "notstarted" and status == "inprogress":
-                print(eventId, "START")
+                print(eventId, "KICK-OFF")
+                print("League:", leagueName)
                 print(homeTeam, "VS", awayTeam)
-                # yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "START - " + homeTeam + " VS " + awayTeam)
+                messageText = "KICK-OFF" + "\n" + "League: " + leagueName + "\n" + homeTeam + " VS " + awayTeam
 
             # Check is match has updated score
             elif status == "inprogress" and (LOG_homeScore != homeScore or LOG_awayScore != awayScore):
-                print(eventId, "SCORE")
-                print(homeTeam, "VS", awayTeam, "@", minute)
+                print(eventId, "GOOOOAL")
+                print(homeTeam, "VS", awayTeam)
+                print("Minute:", minute)
                 print(LOG_homeScore, "-", LOG_awayScore)
                 print(homeScore, "-", awayScore)
-                # yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "SCORE (" + minute + "') - " + homeTeam + "(" + homeScore + ")" + " VS " + awayTeam + "(" + awayScore + ")")
+                messageText = "GOOOOAL" + "\n" + homeTeam + " VS " + awayTeam + "\n" + "Minute: " + str(minute) + "\n" + "SCORE: " + str(homeScore) + " - " + str(awayScore)
 
             # Check if game is half time
             elif LOG_status == "inprogress" and status == "halftime":
                 print(eventId, "HALF-TIME")
                 print(homeTeam, "VS", awayTeam)
-                # yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "HALF-TIME - " + homeTeam + "(" + homeScore + ")" + " VS " + awayTeam + "(" + awayScore + ")")
+                print("SCORE", homeScore, "-", awayScore)
+                messageText = "HALF-TIME" + "\n" + homeTeam + " VS " + awayTeam + "\n" + "SCORE: " + str(homeScore) + " - " + str(awayScore)
 
             # Check if game has resumed
             elif LOG_status == "halftime" and status == "inprogress":
-                print(eventId, "RESUMED")
+                print(eventId, "RESTARTED")
                 print(homeTeam, "VS", awayTeam)
-                # yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "RESUMED - " + homeTeam + "(" + homeScore + ")" + " VS " + awayTeam + "(" + awayScore + ")")
+                print("SCORE", homeScore, "-", awayScore)
+                messageText = "RESTARTED" + "\n" + homeTeam + " VS " + awayTeam + "\n" + "SCORE: " + str(homeScore) + " - " + str(awayScore)
 
             # Check if mathc has finished
             elif LOG_status == "inprogress" and status == "finished":
                 print(eventId, "FULL-TIME")
                 print(homeTeam, "VS", awayTeam)
-                print(homeScore, "-", awayScore)
-                # yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "FULL-TIME - " + homeTeam + "(" + homeScore + ")" + " VS " + awayTeam + "(" + awayScore + ")")
+                print("SCORE", homeScore, "-", awayScore)
+                messageText = "FULL-TIME" + "\n" + homeTeam + " VS " + awayTeam + "\n" + "SCORE: " + str(homeScore) + " - " + str(awayScore)
+
+            if messageText:
+                telegramBot = Bot(TELEGRAM_TOKEN)
+                message = telegramBot.send_message(text=messageText, chat_id=TELEGRAM_USER_ID, reply_to_message_id=LOG_messageId)
+                print(message["message_id"])
+                match["messageId"] = message["message_id"]
 
             # Update match on JSON_REPORT is game hasn't finished
             if status == "finished":
@@ -143,7 +170,7 @@ if __name__ == "__main__":
     print("----------------------------------------------------")
     print(str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")))
 
-    # Set temp folder
+    # Set LOG_FILES
     LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log.json")
 
     # Check if script is already running
